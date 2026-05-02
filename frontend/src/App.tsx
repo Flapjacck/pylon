e8152fc943848127f40469413d521a3f2d8cb606
Flapjacck <plkelly333@me.com>
2026-05-02 10:24:15 -0400

    Feat: simple health check component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 
-function App() {
+import { StatusChecker } from './components/StatusChecker';
 
+function App() {
   return (
     <>
-      
+      <StatusChecker />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
